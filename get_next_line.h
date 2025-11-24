e1b6564b30bf40ea14009415c02d165658d71320
isbenmou <ismailbenmoussa203@gmail.com>
2025-11-24 11:13:35 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,9 +1,13 @@
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
-#include <unistd.h>
-#include <stdlib.h>
-int ft_strlen(char *str);
-char *ft_strdup(char *str);
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char *s1, char *s2);
-char *get_next_line(int fd);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+
 #endif
