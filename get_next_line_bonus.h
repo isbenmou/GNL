d7560330cbcbbfd44be4725466333380ecfa1c79
isbenmou <ismailbenmoussa203@gmail.com>
2025-12-01 06:13:12 +0100

    Update get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,23 +1,18 @@
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-# include <unistd.h>
 # include <stdlib.h>
-# include <limits.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
-# if BUFFER_SIZE < 0
-#  undef BUFFER_SIZE
-#  define BUFFER_SIZE 0
-# endif
-
-char	*get_next_line(int fd);
-size_t	ft_strlen(const char *s);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strdup(const char *s);
-char	*ft_strchr(const char *s, int c);
+char    *get_next_line(int fd);
+size_t  ft_strlen(const char *s);
+void    *ft_memcpy(void *dst, const void *src, size_t n);
+char    *ft_strdup(const char *s);
+char    *ft_strchr(const char *s, int c);
+char    *ft_strjoin(char *s1, char *s2);
 
 #endif
