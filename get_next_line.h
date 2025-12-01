9a4bb67bf00d84f57b53360db653f068d137d58d
isbenmou <ismailbenmoussa203@gmail.com>
2025-12-01 06:06:50 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,13 +1,19 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
 
-char	*get_next_line(int fd);
-size_t	ft_strlen(const char *s);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strdup(const char *s);
-char	*ft_strchr(const char *s, int c);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char    *get_next_line(int fd);
+
+size_t  ft_strlen(const char *s);
+void    *ft_memcpy(void *dst, const void *src, size_t n);
+char    *ft_strdup(const char *s);
+char    *ft_strchr(const char *s, int c);
 
 #endif
+
